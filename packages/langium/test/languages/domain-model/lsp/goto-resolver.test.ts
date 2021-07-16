cbe2d3807eab16f3dce2b86554fc39fa9b5325c4
Kai Salmen <kai.salmen@typefox.io>
2021-07-16 17:19:11 +0200

    Expanded goto-resolver.test and references.ts. Definiton from inside Array is resolved correctly

--- packages/langium/test/languages/domain-model/lsp/goto-resolver.test.ts
+++ packages/langium/test/languages/domain-model/lsp/goto-resolver.test.ts
@@ -1,34 +1,76 @@
+/******************************************************************************
+ * Copyright 2021 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
 import { Diagnostic, Position } from 'vscode-languageserver';
-import { buildRealpath, createLangiumGrammarServices, LangiumDocumentConfiguration, loadUriContent } from '../../../../src';
+import { buildRealpath, createLangiumGrammarServices, LangiumDocument, LangiumDocumentConfiguration, LangiumGrammarServices, loadUriContent } from '../../../../src';
+
+let services: LangiumGrammarServices;
+let document: LangiumDocument;
+
+beforeAll(() => {
+    const realPathFile = buildRealpath('packages/langium', 'test/languages/domain-model/domain-model.langium');
+    const grammarFileContent = loadUriContent(realPathFile);
+    document = LangiumDocumentConfiguration.create(`file:${realPathFile}`, 'langium', 0, grammarFileContent);
+    expect(document).toBeDefined;
+
+    services = createLangiumGrammarServices();
+    const diagnostics: Diagnostic[] = [];
+    expect(services.documents.DocumentBuilder.build(document, diagnostics)).toBeTruthy;
+    expect(diagnostics.length).toEqual(0);
+});
 
 describe('GoToResolver', () => {
-    test('Find Definition', () => {
-        const realPathFile = buildRealpath('packages/langium', 'test/languages/domain-model/domain-model.langium');
-        const grammarFileContent = loadUriContent(realPathFile);
-        const document = LangiumDocumentConfiguration.create(`file:${realPathFile}`, 'langium', 0, grammarFileContent);
-        expect(document).toBeDefined;
-
-        const services = createLangiumGrammarServices();
-        const diagnostics: Diagnostic[] = [];
-        expect(services.documents.DocumentBuilder.build(document, diagnostics)).toBeTruthy;
-        expect(diagnostics.length).toEqual(0);
-
+    test('Find Declaration', () => {
         const goToResolver = services.lsp.GoToResolver;
         const textPos = {
             textDocument: {
                 uri: document.uri
             },
-            // Must find Entity in `    DataType | Entity;`
+            // `    DataType | Entity;` - Must find definiton of Entity
             position: Position.create(15, 16)
         };
         const locationLink = goToResolver.goToDefinition(document, textPos);
-        expect(locationLink.length).toBeGreaterThan(0);
         expect(locationLink.length).toBe(1);
-
-        // find Entity in `Entity:`
         expect(locationLink[0].targetSelectionRange.start.line).toBe(20);
         expect(locationLink[0].targetSelectionRange.start.character).toBe(0);
         expect(locationLink[0].targetSelectionRange.end.line).toBe(20);
         expect(locationLink[0].targetSelectionRange.end.character).toBe(6);
+    });
+
+    test('Find itself', () => {
+        const goToResolver = services.lsp.GoToResolver;
+        const textPos = {
+            textDocument: {
+                uri: document.uri
+            },
+            // `Entity:` - Must find itself
+            position: Position.create(20, 2)
+        };
+        const locationLink = goToResolver.goToDefinition(document, textPos);
+        expect(locationLink.length).toBe(1);
+        expect(locationLink[0].targetSelectionRange.start.line).toBe(20);
+        expect(locationLink[0].targetSelectionRange.start.character).toBe(0);
+        expect(locationLink[0].targetSelectionRange.end.line).toBe(20);
+        expect(locationLink[0].targetSelectionRange.end.character).toBe(6);
+    });
+
+    test('Find Array Element', () => {
+        const goToResolver = services.lsp.GoToResolver;
+        const textPos = {
+            textDocument: {
+                uri: document.uri
+            },
+            // `hidden(WS, SL_COMMENT, ML_COMMENT)` - Must find SL_COMMENT
+            position: Position.create(1, 15)
+        };
+        const locationLink = goToResolver.goToDefinition(document, textPos);
+        expect(locationLink.length).toBe(1);
+        expect(locationLink[0].targetSelectionRange.start.line).toBe(37);
+        expect(locationLink[0].targetSelectionRange.start.character).toBe(9);
+        expect(locationLink[0].targetSelectionRange.end.line).toBe(37);
+        expect(locationLink[0].targetSelectionRange.end.character).toBe(19);
     });
 });
